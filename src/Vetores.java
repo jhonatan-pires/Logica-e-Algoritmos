9f979616bb4eb9f5e5bfa7208e75f8a8f88d59dc
jhonatan <jhonatanpires636@gmail.com>
2021-07-12 07:23:27 -0300

    Aprendendo sobre Vetores

--- src/Vetores.java
+++ src/Vetores.java
@@ -2,12 +2,25 @@ public class Vetores {
     public static void main(String[] args) {
         Double faturamento = 100.0;
 
-        Double[] faturamentoJaneiro = new Double[] {100.0, 200.0, 300.0};
-        Double[] faturamentoFev = new Double[] {200.0, 300.0, 400.0};
+        Double[] faturamentoJaneiro = new Double[]{100.0, 200.0, 300.0};
+        Double[] faturamentoFev = new Double[]{200.0, 300.0, 400.0};
 
 
-        Double[][] faturamentoAnual = new Double[][] {faturamentoJaneiro, faturamentoFev};
+        Double[][] faturamentoAnual = new Double[][]{faturamentoJaneiro, faturamentoFev};
 
-        System.out.println("Faturamento Anual" + faturamentoAnual[0][0]);
+        //System.out.println("Faturamento Anual" + faturamentoAnual[0][0]);
+
+        for (int i = 0; i < faturamentoAnual.length; i++) {
+            System.out.println("Mes" + (i + 1));
+
+
+            Double[] mes = faturamentoAnual[i];
+
+            for (int y = 0; y < mes.length; y++) {
+                Double dia = mes[y];
+                System.out.println("Dia" + y + " :" + dia);
+            }
+
+        }
     }
 }
