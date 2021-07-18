781703d2ba2dd60580b317906e0712364e6c550d
jhonatan <jhonatanpires636@gmail.com>
2021-07-18 20:41:24 -0300

    Melhorando o codigo;

--- src/Cerveja/ReporEstoque.java
+++ src/Cerveja/ReporEstoque.java
@@ -7,15 +7,6 @@ public class ReporEstoque {
         estoque.cerveja = "skol";
         estoque.vodka = 10;
 
-        estoqueDosProdutosConsumidos(estoque);
-
-    }
-    static void estoqueDosProdutosConsumidos(Estoque estoque){
-        if (estoque.vodka >= 10){
-            System.out.println("estoque:" +  estoque.cerveja + "possui:" + estoque.vodka + "Precisa repor estoque!");
-        }else{
-            System.out.println("estoque:" +  estoque.cerveja + "possui:" + estoque.vodka + "Não Precisa repor estoque!");
-        }
-
+        System.out.println("Estoque :" + estoque.estoqueDosProdutosConsumidos());
     }
 }
